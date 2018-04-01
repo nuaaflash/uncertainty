570a32f2fefd649a3ae44621137df78e6521a82b
161530319-XiaHan <xh.yes@qq.com>
2018-04-01 10:48:46 +0800

    模型管理文件夹修改为英文
    试图安装mysql包却失败

---
+++ UncertaintyModeling/src/test2.py
@@ -0,0 +1,15 @@
+from desc import description
+
+def function(x=[], a1=1, a2=1, a3=1, a4=1):
+    print description.description()
+    result = a1 * x[0]**3
+    result += a2 * x[1]**2
+    result += a3 * x[2]
+    result += a4
+    return result
+
+if __name__ == '__main__':
+    variable = [1,2,3]
+    print function(x = variable)
+    
+    

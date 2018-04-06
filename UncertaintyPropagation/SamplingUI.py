a003720539cae347f3ef8a2c8610292f8c149c46
Dengjihao <dengjihao@nuaa.edu.cn>
2018-04-06 10:33:22 +0800

    将窗口改为Dialog
    将窗口改为Dialog

--- UncertaintyPropagation/SamplingUI.py
+++ UncertaintyPropagation/SamplingUI.py
@@ -1,63 +1,46 @@
+# -*- coding: utf-8 -*-
+
 import wx
 import wx.xrc
 import wx.aui
 
-
 ###########################################################################
-## Class MyFrame2
-###########################################################################
-
-# -*- coding: utf-8 -*-
-
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 17 2015)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
+# Class SamplingDialog
 ###########################################################################
 
-import wx
-import wx.xrc
 
-
-###########################################################################
-## Class MyFrame1
-###########################################################################
-
-class MyFrame1(wx.Frame):
+class SamplingDialog(wx.Dialog):
 
 	def __init__(self, parent):
-		wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=wx.EmptyString, pos=wx.DefaultPosition,
-						  size=wx.Size(228, 300), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+		wx.Dialog.__init__(self, parent, id=wx.ID_ANY, title=u"Argument", pos=wx.DefaultPosition,
+						   size=wx.Size(230, 242), style=wx.DEFAULT_DIALOG_STYLE)
 
 		self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
 
-		sbSizer1 = wx.StaticBoxSizer(wx.StaticBox(self, wx.ID_ANY, u"Input the arguments"), wx.VERTICAL)
+		bSizer1 = wx.BoxSizer(wx.VERTICAL)
 
-		self.m_staticText1 = wx.StaticText(sbSizer1.GetStaticBox(), wx.ID_ANY, u"Arg1", wx.DefaultPosition,
-										   wx.DefaultSize, 0)
-		self.m_staticText1.Wrap(-1)
-		sbSizer1.Add(self.m_staticText1, 0, wx.ALL, 5)
+		self.m_staticText3 = wx.StaticText(self, wx.ID_ANY, u"Arg1", wx.DefaultPosition, wx.DefaultSize, 0)
+		self.m_staticText3.Wrap(-1)
+		bSizer1.Add(self.m_staticText3, 0, wx.ALL, 5)
 
-		self.m_textCtrl1 = wx.TextCtrl(sbSizer1.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition,
-									   wx.DefaultSize, 0)
-		sbSizer1.Add(self.m_textCtrl1, 0, wx.ALL, 5)
+		# 参数1
+		self.m_textCtrl1 = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+		bSizer1.Add(self.m_textCtrl1, 0, wx.ALL, 5)
 
-		self.m_staticText2 = wx.StaticText(sbSizer1.GetStaticBox(), wx.ID_ANY, u"Arg2", wx.DefaultPosition,
-										   wx.DefaultSize, 0)
-		self.m_staticText2.Wrap(-1)
-		sbSizer1.Add(self.m_staticText2, 0, wx.ALL, 5)
+		self.m_staticText4 = wx.StaticText(self, wx.ID_ANY, u"Arg2", wx.DefaultPosition, wx.DefaultSize, 0)
+		self.m_staticText4.Wrap(-1)
+		bSizer1.Add(self.m_staticText4, 0, wx.ALL, 5)
 
-		self.m_textCtrl2 = wx.TextCtrl(sbSizer1.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition,
-									   wx.DefaultSize, 0)
-		sbSizer1.Add(self.m_textCtrl2, 0, wx.ALL, 5)
+		# 参数2
+		self.m_textCtrl2 = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+		bSizer1.Add(self.m_textCtrl2, 0, wx.ALL, 5)
 
-		self.m_button1 = wx.Button(sbSizer1.GetStaticBox(), wx.ID_ANY, u"OK", wx.DefaultPosition, wx.DefaultSize,
-								   0)
+		self.m_button1 = wx.Button(self, wx.ID_ANY, u"OK", wx.DefaultPosition, wx.DefaultSize, 0)
+		bSizer1.Add(self.m_button1, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+
 		self.m_button1.Bind(wx.EVT_BUTTON, self.getArgs)
-		sbSizer1.Add(self.m_button1, 0, wx.ALL, 5)
 
-		self.SetSizer(sbSizer1)
+		self.SetSizer(bSizer1)
 		self.Layout()
 
 		self.Centre(wx.BOTH)
@@ -74,6 +57,6 @@ class MyFrame1(wx.Frame):
 
 
 app = wx.App(False)
-frame = MyFrame1(None)
+frame = SamplingDialog(None)
 frame.Show()
 app.MainLoop()
